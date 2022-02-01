e5cba718a7be33aaf9361a785e32cf07a3ec3765
tobe1205@hanmail.net <tobe1205@hanmail.net>
2022-02-01 17:51:50 +0900

    [22.02.01] 백준 단계별 문제풀이 - while문

---
+++ src/By_Step_level/step04_while/Q1110/Main.java
@@ -0,0 +1,26 @@
+package By_Step_level.step04_while.Q1110;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int N = in.nextInt();
+        in.close();
+
+        int cnt = 0;
+        int copy = N;
+
+        while (true) {
+            N = ((N % 10) * 10) + (((N / 10) + (N % 10)) % 10);
+            cnt++;
+
+            if (copy == N) {
+                break;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
