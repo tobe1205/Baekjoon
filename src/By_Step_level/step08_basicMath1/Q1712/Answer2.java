839ab063f624e8e433af186cea2746cec8cc283c
tobe1205@hanmail.net <tobe1205@hanmail.net>
2022-03-10 11:45:55 +0900

    [22.03.10] 백준 단계별 문제풀이 - 기본 수학1

---
+++ src/By_Step_level/step08_basicMath1/Q1712/Answer2.java
@@ -0,0 +1,25 @@
+package By_Step_level.step08_basicMath1.Q1712;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Answer2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+
+        if (C <= B) {
+            System.out.println("-1");
+        }
+        else {
+            System.out.println((A/(C-B))+1);
+        }
+    }
+}
---
+++ src/By_Step_level/step08_basicMath1/Q1712/Main.java
@@ -0,0 +1,20 @@
+package By_Step_level.step08_basicMath1.Q1712;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt(); //고정 비용
+        int B = sc.nextInt(); //가변 비용
+        int C = sc.nextInt(); //제품의 가격
+
+        if (C <= B) {
+            System.out.print("-1");
+        } else {
+            System.out.println((A / (C - B)) + 1);
+        }
+    }
+}
