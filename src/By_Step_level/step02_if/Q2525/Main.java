94c0b65306f4798e98a937ac18f71c7e4c171d23
tobe1205@hanmail.net <tobe1205@hanmail.net>
2022-06-27 20:08:22 +0900

    [22.06.27] 백준 단계별 문제풀이 - 조건문

---
+++ src/By_Step_level/step02_if/Q2525/Main.java
@@ -0,0 +1,32 @@
+package By_Step_level.step02_if.Q2525;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int h = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int endTime = Integer.parseInt(br.readLine());
+
+        h += endTime / 60;
+        m += endTime % 60;
+
+        if (m > 59) {
+            h += 1;
+            m -= 60;
+        }
+        if (h > 23) {
+            h -= 24;
+        }
+        System.out.println(h + " " + m);
+    }
+}
